8c0d5ef68edb2015e39a767546b07b13744dbc40
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2024-03-07 23:13:21 -0800

    Learn how to do rust syntax to create a fixed size array (rust also has Vec<T> which we'll see later)

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -5,13 +5,14 @@
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    // Slices are a form of ownership and refer to a contiguous chunk of memory
+    // this is a string slice
+    let nice_slice = &a[1..a.len()-1];
 
+    // no semicolon needed to note the end of a line in Rust
     assert_eq!([2, 3, 4], nice_slice)
 }
