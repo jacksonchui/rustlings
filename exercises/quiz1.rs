48137d4ee455738ad7a61910351f3098b886f7aa
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2024-03-07 23:04:48 -0800

    FINISH: first rust quiz!

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(count: u32) -> u32 {
+    let per_unit_cost = if count > 40 { 1 } else { 2 };
+    count * per_unit_cost
+}
 
 // Don't modify this function!
 #[test]
