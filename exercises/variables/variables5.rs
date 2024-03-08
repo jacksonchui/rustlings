c65f897dbb477a1801caf8fe02ae4981ffc696ae
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2024-03-07 22:31:43 -0800

    Shadowing...scopes are dope

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -3,11 +3,14 @@
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
-    println!("Number plus two is : {}", number + 2);
+    { 
+        // Shadowing is cool, because just like in shell or a crazy
+        // version of scope in cpp, it allows us to access local vars
+        // inside a scope.
+        let number = 3; // don't rename this variable
+        println!("Number plus two is : {}", number + 2);
+    }
 }
