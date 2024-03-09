890d2717b26cde2e1068b500c0949de2c50eafc5
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2024-03-08 18:51:43 -0800

    strings4: working out the difference between String and &str for statics...really nuanced but dynamic alloc if we can't easily slice and pass to specific based on if we need to own or not

--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -7,8 +7,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 fn string_slice(arg: &str) {
     println!("{}", arg);
 }
@@ -17,14 +15,20 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string_slice("blue");
+    string("red".to_string());
+    string(String::from("hi"));
+    // creates owned data from borrowing
+    string("rust is fun!".to_owned());
+    // doesn't convert into different type?
+    string_slice("nice weather".into());
+    string(format!("Interpolation {}", "Station"));
+    // When ref, it is not amendable to being owned
+    string_slice(&String::from("abc")[0..1]);
+    // Creates a sliced ref .. use to_string or from to convert
+    string_slice("  hello there ".trim());
+    // Owns a new bit of memory to replace Mon with Tues
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));
+    // Creates a new String object...not just &'static str
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
