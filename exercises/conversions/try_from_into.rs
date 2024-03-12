7dbef389c31afdc96766df1f54a12210609f7535
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2024-03-12 16:46:40 -0700

    try from into: return a result for the From trait

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -9,7 +9,7 @@
 // Execute `rustlings hint try_from_into` or use the `hint` watch subcommand for
 // a hint.
 
-use std::convert::{TryFrom, TryInto};
+use std::{convert::{TryFrom, TryInto}, ops::RangeInclusive};
 
 #[derive(Debug, PartialEq)]
 struct Color {
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -41,6 +39,8 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (r, g, b): (i16, i16, i16) = tuple;
+        rgb_to_color(r, g, b)
     }
 }
 
@@ -48,6 +48,11 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        // can clone the rgb value underneath
+        let r: i16 = arr.get(0).unwrap().clone();
+        let g: &i16 = arr.get(1).unwrap();
+        let b: &i16 = arr.get(2).unwrap();
+        rgb_to_color(r, *g, *b)
     }
 }
 
@@ -55,7 +60,37 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+
+        let r = slice.get(0).unwrap();
+        let g = slice.get(1).unwrap();
+        let b = slice.get(2).unwrap();
+
+        rgb_to_color(*r, *g, *b)
     }
+}
+
+fn rgb_to_color(r: i16, g: i16, b: i16) -> Result<Color, IntoColorError> {
+        let valid_colors: RangeInclusive<i16> = 0..=255;
+
+        if !valid_colors.contains(&r) {
+            return Err(IntoColorError::IntConversion);
+        }
+        if !valid_colors.contains(&g) {
+            return Err(IntoColorError::IntConversion);
+        }
+        if !valid_colors.contains(&b) {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        // alternatively... num::clamp::<i16>(T, 0, 255);
+        Ok( Color {
+            red: r as u8,
+            green: g as u8,
+            blue: b as u8,
+        })
 }
 
 fn main() {
