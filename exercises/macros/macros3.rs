50017dd3d71d8248d7fece2f1f941a04472a7886
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2024-03-11 23:19:01 -0700

    macros3: macro doesn't end here

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -5,8 +5,9 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+// #[macro_use] attribute is used to make a module's macro scope
+// not end whenthe module is closed
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
